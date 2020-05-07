01057b1e2b717c9cd897bece76fd05565ca34fdb
h.stasiuk <h.stasiuk@zoovu.com>
2020-05-07 19:23:58 +0200

    UnitTests chapter1

---
+++ src/main/java/it/panda/chapter1/FahrenheitCelciusConverter.java
@@ -0,0 +1,13 @@
+package it.panda.chapter1;
+
+public class FahrenheitCelciusConverter {
+
+    public static int toCelcius(int fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public static int toFahrenheit(int celcius) {
+        return celcius * 9 / 5 + 32;
+    }
+
+}
